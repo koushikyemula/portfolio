1e93836620af5d68bfd27737b149907b7fa79922
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-11-20 17:30:30 +0530

    add: footer component

---
+++ app/(main)/(landing)/page.tsx
@@ -0,0 +1,10 @@
+import { Github } from "lucide-react";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      landing page
+      <Github />
+    </main>
+  );
+}
