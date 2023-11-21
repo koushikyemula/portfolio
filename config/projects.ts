9b2540d96170d93257a7073a929dd1969e5b1d74
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-11-21 21:54:06 +0530

    add: links

--- config/projects.ts
+++ config/projects.ts
@@ -2,17 +2,21 @@ export const projects = [
   {
     name: "Crux",
     description: "Just a Notion clone.",
+    url: "https://github.com/BlitZSenpai/Crux",
   },
   {
     name: "Stellar",
     description: "An all-in-one AI tool for code generation, image generation and more..",
+    url: "https://github.com/BlitZSenpai/Stellar",
   },
   {
     name: "Bob",
     description: "A work in progress blog website",
+    url: "https://github.com/BlitZSenpai/blog-test",
   },
   {
     name: "Otto",
     description: "Otto is an appilcation which allows you to have conversations with any PDF document",
+    url: "https://github.com/BlitZSenpai/Otto",
   },
 ];
