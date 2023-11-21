54e69ebe4d9efb8ef17c5e8bb648ae4ab2c3acaa
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-11-21 21:45:37 +0530

    simplified footer component

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,29 +1,6 @@
-import { Github, Linkedin, Mail, Twitter } from "lucide-react";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { socials } from "@/config/socials";
 import Link from "next/link";
-
-const links = [
-  {
-    icon: Github,
-    href: "https://github.com/BlitZSenpai",
-    tooltipContent: "Github",
-  },
-  {
-    icon: Linkedin,
-    href: "https://www.linkedin.com/in/koushik-yemula/",
-    tooltipContent: "Linkedin",
-  },
-  {
-    icon: Mail,
-    href: "https://github.com/BlitZSenpai",
-    tooltipContent: "Mail",
-  },
-  {
-    icon: Twitter,
-    href: "https://twitter.com/koushikyemula",
-    tooltipContent: "X (formarly known as twitter)",
-  },
-];
 
 export const Footer = () => {
   return (
@@ -34,16 +11,16 @@ export const Footer = () => {
           <h2>Koushik Yemula</h2>
         </div>
         <div className="flex items-center justify-center gap-x-4 pr-2">
-          {links.map((link) => (
-            <TooltipProvider key={link.tooltipContent}>
+          {socials.map((social) => (
+            <TooltipProvider key={social.tooltipContent}>
               <Tooltip>
                 <TooltipTrigger asChild>
-                  <Link target="_blank" href={link.href}>
-                    <link.icon className="h-4 w-4" />
+                  <Link target="_blank" href={social.href}>
+                    <social.icon className="h-4 w-4" />
                   </Link>
                 </TooltipTrigger>
                 <TooltipContent className="bg-background">
-                  <p className="text-zinc-400 text-sm">{link.tooltipContent}</p>
+                  <p className="text-zinc-400 text-sm">{social.tooltipContent}</p>
                 </TooltipContent>
               </Tooltip>
             </TooltipProvider>
