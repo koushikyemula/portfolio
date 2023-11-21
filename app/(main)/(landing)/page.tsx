b2d7ee5bbf2df04ae335024fdd16efdbe6afad03
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-11-21 21:57:56 +0530

    add: line height

--- app/(main)/(landing)/page.tsx
+++ app/(main)/(landing)/page.tsx
@@ -8,12 +8,12 @@ export default function Home() {
       </div>
       <div>
         <span className="text-muted-foreground text-xs">IPA /blɪts/ - idk, it sounds cool</span>
-        <p className="text-zinc-100 pb-7 gap-2">
+        <p className="text-zinc-100 pb-7 gap-2 leading-7">
           Hi there, I&apos;m <span className="text-bold">Koushik</span>. I&apos;m a 19y/o CS undergrad student
           who loves to code. I love dark stuff, open source and simple web design. When i&apos;m not coding, i
           like playing games and watching anime.
         </p>
-        <p>
+        <p className="leading-7">
           Right now i&apos;m looking for new opportunities and exploring the world of coding to futher improve
           my skills and knowledge.
         </p>
