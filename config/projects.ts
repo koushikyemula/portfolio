47f3b8608539823df99a97f57d0e8e95a0513f98
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-11-21 21:44:54 +0530

    add: projects config

---
+++ config/projects.ts
@@ -0,0 +1,18 @@
+export const projects = [
+  {
+    name: "Crux",
+    description: "Just a Notion clone.",
+  },
+  {
+    name: "Stellar",
+    description: "An all-in-one AI tool for code generation, image generation and more..",
+  },
+  {
+    name: "Bob",
+    description: "A work in progress blog website",
+  },
+  {
+    name: "Otto",
+    description: "Otto is an appilcation which allows you to have conversations with any PDF document",
+  },
+];
