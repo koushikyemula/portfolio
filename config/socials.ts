4e05c701bdc221812522379385dcf8593477f0c7
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-11-21 21:40:02 +0530

    add: socials config

---
+++ config/socials.ts
@@ -0,0 +1,24 @@
+import { Github, Linkedin, Mail, Twitter } from "lucide-react";
+
+export const socials = [
+  {
+    icon: Github,
+    href: "https://github.com/BlitZSenpai",
+    tooltipContent: "Github",
+  },
+  {
+    icon: Linkedin,
+    href: "https://www.linkedin.com/in/koushik-yemula/",
+    tooltipContent: "Linkedin",
+  },
+  {
+    icon: Mail,
+    href: "https://github.com/BlitZSenpai",
+    tooltipContent: "Mail",
+  },
+  {
+    icon: Twitter,
+    href: "https://twitter.com/koushikyemula",
+    tooltipContent: "X (formarly known as twitter)",
+  },
+];
